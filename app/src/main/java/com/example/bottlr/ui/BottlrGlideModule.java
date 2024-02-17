b53965210bcf9c18ba1bbcb0113de7d272390bb6
WetwareDulachan <sibirew@gmail.com>
2024-02-17 05:49:07 -0800

    Log functions added to see why Search isn't working but not throwing logcat errors

---
+++ app/src/main/java/com/example/bottlr/ui/BottlrGlideModule.java
@@ -0,0 +1,9 @@
+package com.example.bottlr.ui;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+@GlideModule
+public final class BottlrGlideModule extends AppGlideModule {
+    // Leave empty for now. You can add configurations later if needed.
+}
--- app/src/main/java/com/example/bottlr/ui/search/SearchFragment.java
+++ app/src/main/java/com/example/bottlr/ui/search/SearchFragment.java
@@ -1,11 +1,17 @@
 package com.example.bottlr.ui.search;
 
+import static com.example.bottlr.SharedUtils.parseBottle;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -14,6 +20,9 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.bottlr.Bottle;
 import com.example.bottlr.R;
 import com.example.bottlr.ui.RecyclerView.BottleAdapter;
+import com.example.bottlr.ui.gallery.DetailViewActivity;
+
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
@@ -24,9 +33,8 @@ import java.util.stream.Collectors;
 public class SearchFragment extends Fragment {
 
     private EditText nameField, distilleryField, typeField, abvField, ageField, notesField, regionField, ratingField, keywordsField;
-    private Button searchButton;
-    private RecyclerView searchResultsRecyclerView;
     private BottleAdapter searchResultsAdapter;
+    Button searchButton;
 
     @Nullable
     @Override
@@ -45,18 +53,22 @@ public class SearchFragment extends Fragment {
         searchButton = view.findViewById(R.id.search_button);
 
 
-        searchResultsRecyclerView = view.findViewById(R.id.search_results_recyclerview);
+        RecyclerView searchResultsRecyclerView = view.findViewById(R.id.search_results_recyclerview);
         // Set the LayoutManager
         searchResultsRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         // Initialize your adapter and set it to the RecyclerView
-        searchResultsAdapter = new BottleAdapter(new ArrayList<>(), this::onBottleSelected);
+        searchResultsAdapter = new BottleAdapter(new ArrayList<>(), this::searchedBottleSelected);
         searchResultsRecyclerView.setAdapter(searchResultsAdapter);
 
-        searchButton.setOnClickListener(v -> performSearch());
+        searchButton.setOnClickListener(v -> {
+            performSearch();
+            Log.d("SearchFragment", "Search button clicked");
+        });
         return view;
     }
 
     private void performSearch() {
+        Log.d("SearchFragment", "performSearch() called");
         // Get search criteria from user input
         String name = nameField.getText().toString().toLowerCase();
         String distillery = distilleryField.getText().toString().toLowerCase();
@@ -70,8 +82,9 @@ public class SearchFragment extends Fragment {
 
         Set<String> searchKeywords = new HashSet<>(Arrays.asList(keywordsInput.split("\\s*,\\s*")));
 
-        // Assuming getBottles() retrieves your full list of Bottle objects
-        List<Bottle> allBottles = getBottles();
+        // getBottlesToSearch() should retrieve the full list of Bottle objects
+        List<Bottle> allBottles = getBottlesToSearch();
+        Log.d("SearchFragment", "All bottles: " + allBottles);
 
         // Filter the list based on search criteria
         List<Bottle> filteredList = allBottles.stream()
@@ -85,6 +98,7 @@ public class SearchFragment extends Fragment {
                 .filter(bottle -> bottle.getRating().toLowerCase().contains(rating))
                 .filter(bottle -> bottle.getKeywords().stream().anyMatch(keyword -> searchKeywords.contains(keyword.toLowerCase())))
                 .collect(Collectors.toList());
+        Log.d("SearchFragment", "Filtered list: " + filteredList);
         updateSearchResults(filteredList);
     }
 
@@ -93,13 +107,31 @@ public class SearchFragment extends Fragment {
         searchResultsAdapter.notifyDataSetChanged();
     }
 
-    private List<Bottle> getBottles() {
-        // Implement this method to return your actual list of bottles
-        return new ArrayList<>();
-    }
+    // Get the list of bottles to search
+    private List<Bottle> getBottlesToSearch() {
+        List<Bottle> bottles = new ArrayList<>();
+        File directory = getContext().getFilesDir();
+        File[] files = ((File) directory).listFiles();
 
-    // Example method to handle bottle selection from the search results
-    private void onBottleSelected(int position) {
-        // Handle the bottle selection
+        for (File file : files) {
+            if (file.isFile() && file.getName().startsWith("bottle_")) {
+                Bottle bottle = parseBottle(file);
+                if (bottle != null) {
+                    // Make sure the bottle actually exists
+                    bottles.add(bottle);
+                }
+            }
+        }
+        Log.d("SearchFragment", "Bottles to search: " + bottles);
+        return bottles;
+    }
+    // Handle bottle selection
+    public void searchedBottleSelected(int position) {
+        Bottle selectedBottle = searchResultsAdapter.getBottle(position);
+        if (selectedBottle != null) {
+            Intent intent = new Intent(getActivity(), DetailViewActivity.class);
+            intent.putExtra("selectedBottle", selectedBottle);
+            startActivity(intent);
+        }
     }
 }
