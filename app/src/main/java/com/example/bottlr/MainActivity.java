2a520af269415b4af9ada3821d400cd889a527f2
Mike Legend <michael.george.why@gmail.com>
2024-06-03 17:10:41 -0700

    refactor recycler part 2

--- app/src/main/java/com/example/bottlr/MainActivity.java
+++ app/src/main/java/com/example/bottlr/MainActivity.java
@@ -1,8 +1,19 @@
 package com.example.bottlr;
 
+import static com.example.bottlr.SharedUtils.queryBuilder;
+import static com.example.bottlr.SharedUtils.saveImageToGallery;
+import static com.example.bottlr.SharedUtils.shareBottleInfo;
+import static com.example.bottlr.SharedUtils.showDeleteConfirm;
+
 import android.animation.ObjectAnimator;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -10,6 +21,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.DividerItemDecoration;
+
+import com.bumptech.glide.Glide;
+import com.example.bottlr.ui.gallery.DetailViewActivity;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.appcheck.FirebaseAppCheck;
 import com.google.firebase.appcheck.playintegrity.PlayIntegrityAppCheckProviderFactory;
@@ -21,6 +35,8 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private AppBarConfiguration mAppBarConfiguration;
+    public List<Bottle> bottles;
+    public List<Bottle> allBottles;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -60,22 +76,112 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.detail_view_activity);
     }*/
 
-    /*void GenerateLiquorRecycler() {
+    ImageButton backButton;
+    ImageButton deleteButton;
+    ImageButton shareButton;
+    ImageButton buyButton;
+    ImageButton editButton;
+    ImageButton saveImageButton;
+
+    void GenerateLiquorRecycler() {
         // Set Recycler
         RecyclerView LiquorCabinetRecycler = findViewById(R.id.liquorRecycler);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         LiquorCabinetRecycler.setLayoutManager(layoutManager);
-        List<Bottle> bottles = SharedUtils.loadBottles(this);
 
+        //List<Bottle> bottles = SharedUtils.loadBottles(this);
         // Line divider to keep things nice and neat
         DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(this, layoutManager.getOrientation());
         LiquorCabinetRecycler.addItemDecoration(dividerItemDecoration);
 
         // Bottle listing
-        BottleAdapter liquorAdapter = new BottleAdapter(bottles, this);
+        BottleAdapter liquorAdapter;
+        liquorAdapter = new BottleAdapter(bottles, allBottles, new BottleAdapter.OnBottleCheckListener() {
+            @Override
+            public void onButtonClick(String string) { detailedView(string); }
+        });
         LiquorCabinetRecycler.setAdapter(liquorAdapter);
         liquorAdapter.notifyDataSetChanged();
-    }*/
+    }
+
+    public void detailedView(String string) {
+        setContentView(R.layout.detail_view_activity);
+
+        /*// Find the views
+        ImageView bottleImage = findViewById(R.id.detailImageView);
+        bottleImage.setScaleType(ImageView.ScaleType.FIT_CENTER); // Set the scale type of the ImageView so it displays properly
+        // view Initialization
+        TextView bottleName = findViewById(R.id.tvBottleName);
+        TextView bottleDistillery = findViewById(R.id.tvDistillery);
+        TextView bottleRating = findViewById(R.id.tvRating);
+        TextView bottleDetails = findViewById(R.id.tvBottleDetails);
+        TextView bottleNotes = findViewById(R.id.tvNotes);
+        TextView bottleKeywords = findViewById(R.id.tvKeywords);
+        deleteButton = findViewById(R.id.deleteButton);
+        shareButton = findViewById(R.id.shareButton);
+        buyButton = findViewById(R.id.buyButton);
+        editButton = findViewById(R.id.editButton);
+        backButton = findViewById(R.id.backButton);
+        saveImageButton = findViewById(R.id.saveImageButton);
+
+        // Get the bottle from the intent
+        Bottle bottle = getIntent().getParcelableExtra("selectedBottle");
+
+        // Set the bottle details to the views
+
+        // Glide
+        assert bottle != null;
+        if (bottle.getPhotoUri() != null && !bottle.getPhotoUri().toString().equals("No photo")) {
+            Glide.with(this)
+                    .load(bottle.getPhotoUri())
+                    .error(R.drawable.nodrinkimg) // Default image in case of error
+                    .into(bottleImage);
+        }
+        // Image not working debugging code
+        Log.d("DetailViewActivity", "Image URI: " + bottle.getPhotoUri());
+        // Other fields
+        bottleName.setText(bottle.getName());
+        bottleDistillery.setText(bottle.getDistillery());
+        String rating = bottle.getRating() + " / 10";
+        bottleRating.setText(rating);
+        String details = bottle.getType() + ", " + bottle.getRegion() + ", " + bottle.getAge() + " Year, " + bottle.getAbv() + "% ABV";
+        bottleDetails.setText(details);
+        bottleNotes.setText(bottle.getNotes());
+        String keywords = "Keywords:\n" + String.join(", ", bottle.getKeywords());
+        bottleKeywords.setText(keywords);
+
+        // Delete button listener
+        deleteButton.setOnClickListener(v -> showDeleteConfirm(bottle, this));
+
+        // Buy button listener
+        buyButton.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("https://www.google.com/search?tbm=shop&q=" + Uri.encode(queryBuilder(bottle))));
+            startActivity(intent);
+        });
+
+        // Share button listener
+        shareButton.setOnClickListener(view -> shareBottleInfo(bottle, this));
+
+        // Edit button listener
+        editButton.setOnClickListener(view -> {
+            // Call AddABottle to reuse existing assets
+            Intent intent = new Intent(DetailViewActivity.this, AddABottle.class);
+            intent.putExtra("bottle", bottle);
+            startActivity(intent);
+        });
+
+        // Back button listener
+        backButton.setOnClickListener(v -> finish());
+
+        // Save image button listener
+        saveImageButton.setOnClickListener(v -> {
+            if (bottle.getPhotoUri() != null) {
+                // Save the image to the user's gallery
+                saveImageToGallery(DetailViewActivity.this, bottle);
+            }
+        });*/
+    }
 
     private void animateObject(int id, float start, float finish, int time) { //horizontal constraint animation
         ConstraintLayout navMenu = findViewById(id);
